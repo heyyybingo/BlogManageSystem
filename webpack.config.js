d5576aff06a5a0f1f28518df69824cdfac57779f
heyyybingo <283385508@qq.com>
2020-05-22 17:59:16 +0800

    reduce size

--- webpack.config.js
+++ webpack.config.js
@@ -62,9 +62,9 @@ module.exports = {
 
 
     ],
-    externals: {
-        'vue': 'Vue',
-        // 'element-ui': 'ElEMENT',
-        'element-ui': 'ElementUI',
-    }
+    // externals: {
+    //     'vue': 'Vue',
+    //     // 'element-ui': 'ElEMENT',
+    //     'element-ui': 'ElementUI',
+    // }
 }
